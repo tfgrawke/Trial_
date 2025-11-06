a927aad7501eb0179fbf92b9ab6c090f00945cdd
tfgrawke <tfgrawke@users.noreply.github.com>
2025-11-06 17:29:41 +0000

    Add core-bridge-core.rs, module.py

---
+++ controller-database/template-notification-database/build/core-bridge-core.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/trial_/controller-database/template-notification-database/build/core-bridge-core.rs
+// Generated at: 2025-12-03T23:45:48.426Z
+
+
